eeaefd11767cb7ea3dd18890c4a4373754b039fb
SudeepMI <sudeepm0599@gmail.com>
2022-06-01 01:33:26 +0545

    ..

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const host = process.env.NODE_ENV === "production" ? "https://blood-bank-np.herokuapp.com/" : "http://127.0.0.1:3035";
+const host = process.env.NODE_ENV === "production" ? "https://blood-bank-np.herokuapp.com/api" : "http://127.0.0.1:3035/api";
 export default axios.create({
   baseURL: host,
   headers: {
